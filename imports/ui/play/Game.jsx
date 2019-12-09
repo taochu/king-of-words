a73aa94f6418bb97dc83dff02a2f895fc18a71e3
Tao Chu <b01502043@gmail.com>
2019-12-09 01:30:42 -0500

    word buttons

--- imports/ui/play/Game.jsx
+++ imports/ui/play/Game.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import WordButton from "../components/WordButton";
+import _ from "lodash";
 
 class Game extends React.Component {
-	state = { letters: [] };
+	state = { letters: [], input: "" };
+
 	componentDidMount() {
-		// Generate Game Board (fill letters with 80 letters)
+		const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		const letters = [];
+		for (const i in _.range(100)) {
+			letters.push(alphabets[Math.floor(Math.random() * 25)]);
+		}
+		this.setState({ letters });
 	}
 	renderGameButtons() {
 		const { letters } = this.state;
+		return (
+			<div className="game-grid">
+				{letters.map((letter, i) => {
+					return <WordButton key={i} letter={letter} />;
+				})}
+			</div>
+		);
 	}
 	render() {
 		return (
-			<div>
-				<WordButton letter="k" />
+			<div style={{ textAlign: "center" }}>
+				<div style={{ display: "inline-block" }}>
+					{this.renderGameButtons()}
+				</div>
 			</div>
 		);
 	}
