a40ed7babc0eac56ecb36f090a8b815273a0a54a
Tao Chu <b01502043@gmail.com>
2019-12-08 20:50:03 -0500

    create meteor package king-of-words:styles

---
+++ packages/styles/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+	name: "king-of-words:styles",
+	summary: "king of words style",
+	version: "0.0.1"
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom("METEOR@1.3");
+	api.use(["fourseven:scss"], "client");
+	api.addFiles(
+		[
+			// Bottelio theme common imports
+			"client/imports/_mixins.import.scss",
+			"client/imports/_helpers.import.scss"
+		],
+		["client"],
+		{ isImport: true }
+	);
+
+	api.addFiles(["client/styles/app.scss"], ["client"]);
+});
